9b412ca73a23cd13e59166861e65da1912eadf56
measuredmoth <measuredmoth@protonmail.com>
2026-04-18 13:19:54 -0700

    Added cmd arguments

--- GestureEncryptor.py
+++ GestureEncryptor.py
@@ -6,6 +6,24 @@ from mediapipe.tasks.python.vision.drawing_utils import draw_landmarks
 from GestureHandler import GestureHandler
 from VideoStream import VideoStream
 
+parser = argparse.ArgumentParser(
+    prog="Gesture Encryptor",
+    description="Encrypts and decrypts files as .gesture by using hand gestures"
+)
+
+parser.add_argument("filename")
+parser.add_argument("--encrypt", action="store_true")
+parser.add_argument("--decrypt", action="store_true")
+
+args = parser.parse_args()
+
+filename = args.filename
+encrypt = args.encrypt
+decrypt = args.decrypt
+
+if encrypt and decrypt:
+    raise Exception("Can't both be encrypting and decrypting!")
+
 vidCapture = cv2.VideoCapture(0)
 
 videoStreamer = VideoStream(vidCapture)
